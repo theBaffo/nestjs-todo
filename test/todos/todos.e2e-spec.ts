01270ead5652e91d5c496be14f04f3bf0cbb8362
Marco Giuliani <marco@oozou.com>
2021-07-06 17:06:58 +0700

    e2e tests - add negative tests

--- test/todos/todos.e2e-spec.ts
+++ test/todos/todos.e2e-spec.ts
@@ -7,6 +7,8 @@ import { TodosModule } from '../../src/todos/todos.module';
 import { Todo } from '../../src/todos/entities/todo.entity';
 import { Subtask } from '../../src/todos/entities/subtask.entity';
 import { TodoStatus } from '../../src/todos/entities/todo-status';
+import { TodoNotFoundException } from '../../src/todos/exceptions/todo.not-found.exception';
+import { SubtaskNotFoundException } from '../../src/todos/exceptions/subtask.not-found.exception';
 
 import * as faker from 'faker';
 import * as request from 'supertest';
@@ -72,6 +74,19 @@ describe('Todos', () => {
         },
       ]);
     });
+
+    // Negative tests
+
+    it('should return an empty array', async () => {
+      const { body } = await request
+        .agent(app.getHttpServer())
+        .get('/todos')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(body).toEqual([]);
+    });
   });
 
   describe('GET /todos/:id', () => {
@@ -96,6 +111,26 @@ describe('Todos', () => {
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
         subtasks: [],
+      });
+    });
+
+    // Negative tests
+
+    it("should return 'todo not found'", async () => {
+      const id = faker.datatype.uuid();
+
+      // Run your end-to-end test
+      const { body } = await request
+        .agent(app.getHttpServer())
+        .get(`/todos/${id}`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(404);
+
+      expect(body).toEqual({
+        error: '404',
+        message: new TodoNotFoundException(id).message,
+        statusCode: 404,
       });
     });
   });
@@ -148,6 +183,27 @@ describe('Todos', () => {
         subtasks: [],
       });
     });
+
+    // Negative tests
+
+    it("should return 'todo not found'", async () => {
+      const id = faker.datatype.uuid();
+
+      // Run your end-to-end test
+      const { body } = await request
+        .agent(app.getHttpServer())
+        .patch(`/todos/${id}`)
+        .set('Accept', 'application/json')
+        .send({ status: TodoStatus.COMPLETED })
+        .expect('Content-Type', /json/)
+        .expect(404);
+
+      expect(body).toEqual({
+        error: '404',
+        message: new TodoNotFoundException(id).message,
+        statusCode: 404,
+      });
+    });
   });
 
   describe('DEL /todos/:id', () => {
@@ -171,6 +227,26 @@ describe('Todos', () => {
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
         subtasks: [],
+      });
+    });
+
+    // Negative tests
+
+    it("should return 'todo not found'", async () => {
+      const id = faker.datatype.uuid();
+
+      // Run your end-to-end test
+      const { body } = await request
+        .agent(app.getHttpServer())
+        .del(`/todos/${id}`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(404);
+
+      expect(body).toEqual({
+        error: '404',
+        message: new TodoNotFoundException(id).message,
+        statusCode: 404,
       });
     });
   });
@@ -198,8 +274,6 @@ describe('Todos', () => {
         .expect('Content-Type', /json/)
         .expect(200);
 
-      console.log('body', body);
-
       expect(body).toEqual([
         {
           id: expect.any(String),
@@ -209,6 +283,19 @@ describe('Todos', () => {
           updatedAt: expect.any(String),
         },
       ]);
+    });
+
+    // Negative tests
+
+    it('should return an empty array', async () => {
+      const { body } = await request
+        .agent(app.getHttpServer())
+        .get('/subtasks')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(body).toEqual([]);
     });
   });
 
@@ -243,6 +330,26 @@ describe('Todos', () => {
         updatedAt: expect.any(String),
       });
     });
+
+    // Negative tests
+
+    it("should return 'subtask not found'", async () => {
+      const id = faker.datatype.uuid();
+
+      // Run your end-to-end test
+      const { body } = await request
+        .agent(app.getHttpServer())
+        .get(`/subtasks/${id}`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(404);
+
+      expect(body).toEqual({
+        error: '404',
+        message: new SubtaskNotFoundException(id).message,
+        statusCode: 404,
+      });
+    });
   });
 
   describe('POST /subtasks', () => {
@@ -273,6 +380,28 @@ describe('Todos', () => {
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
         todo: expect.any(Object),
+      });
+    });
+
+    // Negative tests
+
+    it("should return 'todo not found'", async () => {
+      const todoId = faker.datatype.uuid();
+      const title = faker.lorem.sentence();
+
+      // Run your end-to-end test
+      const { body } = await request
+        .agent(app.getHttpServer())
+        .post(`/subtasks`)
+        .set('Accept', 'application/json')
+        .send({ title, todoId })
+        .expect('Content-Type', /json/)
+        .expect(404);
+
+      expect(body).toEqual({
+        error: '404',
+        message: new TodoNotFoundException(todoId).message,
+        statusCode: 404,
       });
     });
   });
@@ -309,6 +438,27 @@ describe('Todos', () => {
         updatedAt: expect.any(String),
       });
     });
+
+    // Negative tests
+
+    it("should return 'subtask not found'", async () => {
+      const id = faker.datatype.uuid();
+
+      // Run your end-to-end test
+      const { body } = await request
+        .agent(app.getHttpServer())
+        .patch(`/subtasks/${id}`)
+        .set('Accept', 'application/json')
+        .send({ status: TodoStatus.COMPLETED })
+        .expect('Content-Type', /json/)
+        .expect(404);
+
+      expect(body).toEqual({
+        error: '404',
+        message: new SubtaskNotFoundException(id).message,
+        statusCode: 404,
+      });
+    });
   });
 
   describe('DEL /subtasks/:id', () => {
@@ -339,6 +489,24 @@ describe('Todos', () => {
         status: TodoStatus.PENDING,
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
+      });
+    });
+
+    it("should return 'subtask not found'", async () => {
+      const id = faker.datatype.uuid();
+
+      // Run your end-to-end test
+      const { body } = await request
+        .agent(app.getHttpServer())
+        .del(`/subtasks/${id}`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(404);
+
+      expect(body).toEqual({
+        error: '404',
+        message: new SubtaskNotFoundException(id).message,
+        statusCode: 404,
       });
     });
   });
